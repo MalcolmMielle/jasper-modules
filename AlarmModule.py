30497ef35071cf61d0b66e17fdcd7eeabc6a7e3c
MalcolmMielle <malcolm.mielle@gmail.com>
2014-11-05 02:52:25 +0100

    made a thread out of the alarm process and add user input by voice command.

--- AlarmModule.py
+++ AlarmModule.py
@@ -1,15 +1,40 @@
 '''
-I should make a passive silent Module, see Jasper API
+Made it a thread.
 '''
 
 # -*- coding: utf-8-*-
 import datetime
 import re
 import os
+from threading import Thread, Lock
 from app_utils import getTimezone
 from semantic.dates import DateService
 
 WORDS = ["ALARM"]
+
+hours = []
+lock = Lock()
+
+def alarmclock(profile, mic,*args):
+	print 'set up !'
+	finished=1
+	while(finished == 1):
+		tz = getTimezone(profile)
+		now = datetime.datetime.now(tz=tz)
+		lock.acquire(True)
+		for i, (h, m) in enumerate (hours):
+			hour_now = now.hour
+			min_now = now.minute
+			#mic.say("it is now %s %s and alarm is %s %s" % (hour_now, min_now, h, m))
+
+			if(hour_now==h and min_now==m):
+				print 'ringing !'
+				#suppress the hour if ringed
+				del hours[i]
+				finished=0
+		lock.release()
+	print 'exit'
+	
 
 def handle(text, mic, profile):
 	"""
@@ -21,22 +46,36 @@ def handle(text, mic, profile):
         profile -- contains information related to the user (e.g., phone
                    number)
     """
-	mic.say("Setting up alarm")
+	mic.say("Setting up alarm, what hour do you want it to ring ?")
 	#Setting arguments
 	#Ask for th hour
-	mic.say("Here are the current top headlines. " + all_titles +
-                ". Would you like me to send you these articles? " +
-                "If so, which?")
 	#Listen to the answer
-	handleResponse(mic.activeListen())
-	hour = 21
-	min = 43
+	hour=mic.activeListen()
+	hour=int(hour)
+	
+	mic.say("And minute ?")
+	min=mic.activeListen()
+	min=int(min)
+	
+	lock.acquire(True)
+	hours.append( (hour, min) )
+	lock.release()
+	
+	if(len(hours)==1):
+		Thread(target=alarmclock,args=(profile,mic,1)).start()
 	
 	"""
 		Fork the process. Child is an alarm clock while the parent continue Jasper.
 		Problem with multiple alarm being multiple process instead of having only one with multiple time :S
 	"""
+	thefile = open(os.path.join(os.path.dirname(__file__),'AlarmFile.txt'), 'w')
+	try:
+		thefile.write("hour : %s min : %s\n" % (hour, min))
+	finally:
+		thefile.close()
 	
+	
+	'''
 	newpid = os.fork()
 	if newpid == 0:
 		print 'Your alarm clock is the process ',  os.getpid( )
@@ -55,7 +94,7 @@ def handle(text, mic, profile):
 	else:
 		pids = (os.getpid(), newpid)
 		print "parent: %d, child: %d" % pids
-	
+	'''
 	
 	
 def isValid(text):
